84c808a19f0221969e413c9c6fea3ae9bc66ea5f
pooriya <adibradp@gmail.com>
2024-10-01 01:57:29 +0330

    sqlalchemy setting be set for interaction

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 import os
 import requests
-
+import models
 # access to environment variable
 load_dotenv()
 # get api token for request to https://openweathermap.org/
@@ -11,11 +13,16 @@ lat_long = [(51.507351, -0.127758), (53.408371, -2.991573), (40.712776, -74.0059
             (38.893883, -77.044142), (43.686677, -79.392166), (49.243843, -123.112445), (35.689133, 51.389524),
             (36.470552, 52.348881), (36.545611, 52.684186), (48.136583, 11.572022), (49.461167, 11.071938)]
 
-
 def request_api(lat_long_var):
     """
     a tuple that carries Latitude and Longitude:param lat_long_var:tuple
-    a json that carries all data like weather , city and etc.:return: json
+    a json that carries all data like weather , city , etc.:return: json
     """
     return requests.get(
         f'https://api.openweathermap.org/data/2.5/forecast?lat={lat_long_var[0]}&lon={lat_long_var[1]}&appid={api_key}')
+"""connection to data base"""
+engine = create_engine(os.environ.get("DATABASE_URL"))
+"""interaction with database"""
+Session = sessionmaker(bind=engine)
+session = Session()
+
