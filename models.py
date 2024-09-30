18fc5542eb41b4254b6a1e7291f4117ab5671436
pooriya <adibradp@gmail.com>
2024-10-01 01:03:54 +0330

    City model be set

--- models.py
+++ models.py
@@ -12,5 +12,21 @@ engine = create_engine(os.environ.get("DATABASE_URL"))
 # base model for inherite to my models
 Base = declarative_base()
 
+"""my model for designing DataBase"""
+
+
+class City(Base):
+    """City model for stored cities data"""
+    __tablename__ = 'city'
+    id = Column(Integer, primary_key=True)
+    city_name = Column(String)
+    country = Column(String)
+    coord_lat = Column(Float)
+    coord_lon = Column(Float)
+
+    def __repr__(self):
+        return f'{self.city_name} - {self.country} '
+
+
 """Table Builder"""
 Base.metadata.create_all(engine)
