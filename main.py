190e6b974f72824760fa9f8ef36343e0d37de45c
pooriya <adibradp@gmail.com>
2024-10-01 01:52:26 +0330

    request api funcation be set

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import os
 import requests
+
 # access to environment variable
 load_dotenv()
 # get api token for request to https://openweathermap.org/
@@ -11,4 +12,10 @@ lat_long = [(51.507351, -0.127758), (53.408371, -2.991573), (40.712776, -74.0059
             (36.470552, 52.348881), (36.545611, 52.684186), (48.136583, 11.572022), (49.461167, 11.071938)]
 
 
-
+def request_api(lat_long_var):
+    """
+    a tuple that carries Latitude and Longitude:param lat_long_var:tuple
+    a json that carries all data like weather , city and etc.:return: json
+    """
+    return requests.get(
+        f'https://api.openweathermap.org/data/2.5/forecast?lat={lat_long_var[0]}&lon={lat_long_var[1]}&appid={api_key}')
